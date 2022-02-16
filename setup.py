3785f78d3a1b1c3b692a1e05385000559990fd36
csonglocale <csong@locale.app>
2022-02-16 12:19:13 +0000

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Local-E-Rewards-ML/itpminer',
-    version='0.0.11',
+    version='0.0.12',
     zip_safe=False,
 )
